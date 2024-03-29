19927068885416dd996b3ce374ff9363ab91e83a
custommoddeller <163665395+custommoddeller@users.noreply.github.com>
2024-03-29 21:05:22 +0000

    Update FreeUnlocks.lua

--- FreeUnlocks.lua
+++ FreeUnlocks.lua
@@ -1,11 +1,11 @@
 while task.wait() do
-    if getgenv().chestSpeed then
+    if getgenv().luckMultiplier then
         game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("Multiplier"):InvokeServer(unpack({
             [1] = "LuckMultiplier",
             [2] = 0.9
         }))
     end
-    if getgenv().luckMultiplier then
+    if getgenv().chestSpeed then
         game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("Multiplier"):InvokeServer(unpack({
             [1] = "ChestSpeedMultiplier",
             [2] = 0.9
