b38b46519b45d64fe5cbd6cfe5fdee889a75601f
custommoddeller <163665395+custommoddeller@users.noreply.github.com>
2024-03-29 21:04:43 +0000

    Create FreeUnlocksLoad.lua

---
+++ FreeUnlocksLoad.lua
@@ -0,0 +1,4 @@
+getgenv().chestSpeed = false
+getgenv().luckMultiplier = true
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/custommoddeller/SuperPowerFightingSimulator/main/FreeUnlocks.lua", true))()
