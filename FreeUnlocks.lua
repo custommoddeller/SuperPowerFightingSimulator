e2a34c8d340ef09555c9deb465e84dac7a8c6c47
custommoddeller <163665395+custommoddeller@users.noreply.github.com>
2024-03-29 21:19:09 +0000

    Update FreeUnlocks.lua

--- FreeUnlocks.lua
+++ FreeUnlocks.lua
@@ -1,14 +1,35 @@
-while task.wait() do
-    if getgenv().luckMultiplier then
+function increaseX(x: String, t: Number)
+    for = 1, t do
         game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("Multiplier"):InvokeServer(unpack({
-            [1] = "LuckMultiplier",
-            [2] = 0.9
-        }))
-    end
-    if getgenv().chestSpeed then
-        game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("Multiplier"):InvokeServer(unpack({
-            [1] = "ChestSpeedMultiplier",
+            [1] = x,
             [2] = 0.9
         }))
     end
 end
+
+local creditThing = Instance.new("BindableFunction")
+
+function creditThing.OnInvoke(response)
+    if response == "Copy username" then
+        setclipboard("astria5")
+    end
+end
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+	Title = "Made by astria5";
+	Text = "Credits to astria5 for making the script";
+	Duration = 5;
+	Button1 = "Copy";
+	Button2 = "Dismiss";
+})
+
+task.spawn(function()
+    while task.wait() do
+        if getgenv().luckMultiplier then
+            increaseX("LuckMultiplier", getgenv().speed_MAY_INCREASE_LAG_IF_TOO_HIGH)
+        end
+        if getgenv().chestSpeed then
+            increaseX("ChestSpeedMultiplier", getgenv().speed_MAY_INCREASE_LAG_IF_TOO_HIGH)
+        end
+    end
+end)
