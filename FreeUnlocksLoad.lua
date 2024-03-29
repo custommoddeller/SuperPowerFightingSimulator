42850e73c8a23712358ca1324de98f414bd925ef
custommoddeller <163665395+custommoddeller@users.noreply.github.com>
2024-03-29 21:19:24 +0000

    Update FreeUnlocksLoad.lua

--- FreeUnlocksLoad.lua
+++ FreeUnlocksLoad.lua
@@ -1,4 +1,5 @@
 getgenv().chestSpeed = false
 getgenv().luckMultiplier = true
+getgenv().speed_MAY_INCREASE_LAG_IF_TOO_HIGH = 5
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/custommoddeller/SuperPowerFightingSimulator/main/FreeUnlocks.lua", true))()
