83d1c5e492e2471b25d97c168afbc864893c5395
custommoddeller <163665395+custommoddeller@users.noreply.github.com>
2024-03-29 21:03:07 +0000

    Update FreeUnlocks.lua

--- FreeUnlocks.lua
+++ FreeUnlocks.lua
@@ -1,12 +1,17 @@
---GAME LINK: https://www.roblox.com/games/2619187362/Super-Power-Fighting-Simulator
+getgenv().chestSpeed = true
+getgenv().luckMultiplier = true
 
 while task.wait() do
-    game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("Multiplier"):InvokeServer(unpack({
-        [1] = "LuckMultiplier",
-        [2] = 0.9
-    }))
-    game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("Multiplier"):InvokeServer(unpack({
-        [1] = "ChestSpeedMultiplier",
-        [2] = 0.9
-    }))
+    if getgenv().chestSpeed then
+        game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("Multiplier"):InvokeServer(unpack({
+            [1] = "LuckMultiplier",
+            [2] = 0.9
+        }))
+    end
+    if getgenv().luckMultiplier then
+        game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("Multiplier"):InvokeServer(unpack({
+            [1] = "ChestSpeedMultiplier",
+            [2] = 0.9
+        }))
+    end
 end
